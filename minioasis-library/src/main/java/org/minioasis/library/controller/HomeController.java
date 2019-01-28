c1a8d0df099ce5653458b3ba25d15992a43a63e6
wm_mo <wm_mo@Marvel>
2019-01-29 01:03:11 +0800

    design of home page using bulma

--- minioasis-library/src/main/java/org/minioasis/library/controller/HomeController.java
+++ minioasis-library/src/main/java/org/minioasis/library/controller/HomeController.java
@@ -1,13 +1,17 @@
 package org.minioasis.library.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+		model.addAttribute("now", new Date());
         return "index";
     }
 	
