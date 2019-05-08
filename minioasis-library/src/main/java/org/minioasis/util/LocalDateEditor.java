bdaec403515ecc79814ccecd8dc94d6902f491ba
minioasis <minioasis@gmail.com>
2019-05-08 17:01:16 +0800

    fixed localDateEditor

--- minioasis-library/src/main/java/org/minioasis/util/LocalDateEditor.java
+++ minioasis-library/src/main/java/org/minioasis/util/LocalDateEditor.java
@@ -10,7 +10,11 @@ public class LocalDateEditor extends PropertyEditorSupport {
 
     @Override
     public void setAsText(String text) throws IllegalArgumentException{
-        setValue(LocalDate.parse(text, DATE_FORMATTER));
+    	if(text != null && !text.equals("")) {
+    		setValue(LocalDate.parse(text, DATE_FORMATTER));
+    	}else {
+    		setValue(null);
+    	}   
     }
 
     @Override
--- minioasis-library/src/main/java/org/minioasis/util/LocalDateTimeEditor.java
+++ minioasis-library/src/main/java/org/minioasis/util/LocalDateTimeEditor.java
@@ -1,6 +1,7 @@
 package org.minioasis.util;
 
 import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -10,7 +11,11 @@ public class LocalDateTimeEditor extends PropertyEditorSupport {
 
     @Override
     public void setAsText(String text) throws IllegalArgumentException{
-        setValue(LocalDateTime.parse(text, DATETIME_FORMATTER));
+    	if(text != null && !text.equals("")) {
+    		setValue(LocalDate.parse(text, DATETIME_FORMATTER));
+    	}else {
+    		setValue(null);
+    	}  
     }
 
     @Override
