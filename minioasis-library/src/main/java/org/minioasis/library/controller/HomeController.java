3ba7b445b8356ddd89d6a03b6e03c4cc8e3dbed5
minioasis <minioasis@gmail.com>
2019-11-23 12:49:49 +0800

    : )

--- minioasis-library/src/main/java/org/minioasis/library/controller/HomeController.java
+++ minioasis-library/src/main/java/org/minioasis/library/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
+
 	@GetMapping("/")
     public String home() {
         return "pub/index";
